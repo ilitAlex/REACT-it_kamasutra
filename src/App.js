1b6425c1d737b5f01f20c0b7e55e5aa504bae64f
Alexandra Zaitsava <ilit_alex@tut.by>
2020-12-09 00:18:48 +0300

    add: get-request to server, add UserContainer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import {Route} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import UsersContainer from "./components/Users/UsersContainer";
 
 
 
@@ -14,7 +15,7 @@ const App = () => {
             <div className='app-wrapper'>
                 <Header/>
                 <Navbar/>
-                <div class='app-wrapper-content'>
+                <div className='app-wrapper-content'>
                     <Route
                         path='/dialogs'
                         render={() => <DialogsContainer/>
@@ -23,6 +24,11 @@ const App = () => {
                     <Route
                         path='/profile'
                         render={ () => <Profile/>
+                        }
+                    />
+                    <Route
+                        path='/users'
+                        render={ () => <UsersContainer/>
                         }
                     />
                 </div>
--- src/redux/redux-store.jsx
+++ src/redux/redux-store.jsx
@@ -2,15 +2,17 @@ import {combineReducers, createStore} from "redux";
 import sidebarReducer from "./sidebar-reducer";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
+import usersReducer from "./users-reducer";
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     messagesPages: dialogsReducer,
     sidebar: sidebarReducer,
+    usersPage: usersReducer,
 });
 
 
-const store = createStore(reducers);
+let store = createStore(reducers);
 
 
 export default store;
