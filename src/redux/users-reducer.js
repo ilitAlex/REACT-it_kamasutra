35924e5a8eefefe5709c5ee2f3262eba68c1918a
Alexandra Zaitsava <ilit_alex@tut.by>
2020-12-09 03:05:42 +0300

    refactor: change presentational and container responsibilities in Users-components

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -10,7 +10,7 @@ let initialState = {
     users: [],
     pageSize: 20,
     totalUsersCount: 0,
-    currentPage: 2
+    currentPage: 1
 };
 
 const usersReducer = (state = initialState, action) => {
