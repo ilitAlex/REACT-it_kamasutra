8e503a35cfb973050ba0910e299595e0628d83be
Alexandra Zaitsava <ilit_alex@tut.by>
2020-12-09 04:12:55 +0300

    refactor: mapDispatchToProps

--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -2,12 +2,12 @@ import React from 'react';
 import Users from './Users';
 import { connect } from 'react-redux';
 import {
-    followAC,
-    setCurrentPageAC,
-    setUsersAC,
-    setUsersTotalCountAC,
-    toggleIsFetchingAC,
-    unfollowAC
+    follow,
+    setCurrentPage,
+    setUsers,
+    setTotalUsersCount,
+    toggleIsFetching,
+    unfollow
 } from '../../redux/users-reducer';
 import * as axios from 'axios';
 import Preloader from "../common/Preloader/Preloader";
@@ -67,28 +67,31 @@ let mapStateToProps = (state) => {
     }
 };
 
+//
+// let mapDispatchToProps = (dispatch) => {
+//   return {
+//       follow: (userId) => {
+//           dispatch(followAC(userId))
+//       },
+//       unfollow: (userId) => {
+//           dispatch(unfollowAC(userId))
+//       },
+//       setUsers: (users) => {
+//           dispatch(setUsersAC(users))
+//       },
+//       setCurrentPage: (pageNumber) => {
+//           dispatch(setCurrentPageAC(pageNumber))
+//       },
+//       setTotalUsersCount: (totalCount) => {
+//             dispatch(setUsersTotalCountAC(totalCount))
+//       },
+//       toggleIsFetching: (isFetching) => {
+//           dispatch(toggleIsFetchingAC(isFetching))
+//       }
+//   }
+// };
 
-let mapDispatchToProps = (dispatch) => {
-  return {
-      follow: (userId) => {
-          dispatch(followAC(userId))
-      },
-      unfollow: (userId) => {
-          dispatch(unfollowAC(userId))
-      },
-      setUsers: (users) => {
-          dispatch(setUsersAC(users))
-      },
-      setCurrentPage: (pageNumber) => {
-          dispatch(setCurrentPageAC(pageNumber))
-      },
-      setTotalUsersCount: (totalCount) => {
-            dispatch(setUsersTotalCountAC(totalCount))
-      },
-      toggleIsFetching: (isFetching) => {
-          dispatch(toggleIsFetchingAC(isFetching))
-      }
-  }
-};
-
-    export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
+    export default connect(mapStateToProps, {
+            follow, unfollow, setUsers,
+            setCurrentPage, setTotalUsersCount, toggleIsFetching,
+        })(UsersContainer);
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -59,11 +59,11 @@ const usersReducer = (state = initialState, action) => {
 };
 
 
-export const followAC = (userId) => ({type: FOLLOW, userId});
-export const unfollowAC = (userId) => ({type: UNFOLLOW, userId});
-export const setUsersAC = (users) => ({type: SET_USERS, users});
-export const setCurrentPageAC = (currentPage) => ({type: CURRENT_PAGE, currentPage});
-export const setUsersTotalCountAC = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, count: totalUsersCount});
-export const toggleIsFetchingAC = (isFetching) => ({type: TOGGLE_IS_FETCHING, isFetching});
+export const follow = (userId) => ({type: FOLLOW, userId});
+export const unfollow = (userId) => ({type: UNFOLLOW, userId});
+export const setUsers = (users) => ({type: SET_USERS, users});
+export const setCurrentPage = (currentPage) => ({type: CURRENT_PAGE, currentPage});
+export const setTotalUsersCount = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, count: totalUsersCount});
+export const toggleIsFetching = (isFetching) => ({type: TOGGLE_IS_FETCHING, isFetching});
 
 export default usersReducer;
