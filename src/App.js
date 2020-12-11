f937cf62348e64845818bd01ce45b6cb8943ff6d
Alexandra Zaitsava <ilit_alex@tut.by>
2020-12-11 03:06:06 +0300

    add: withrouter

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
                         }
                     />
                     <Route
-                        path='/profile'
+                        path='/profile/:userId?'
                         render={ () => <ProfileContainer/>
                         }
                     />
